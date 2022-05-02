80cf672cd866179eb84f7a6bdac1ce2a67c749ce
ujik500 <zwilder1313@gmail.com>
2022-05-01 20:43:30 -0700

    updates to additiontester

---
+++ additiontester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class additiontester {
+    @Test
+    public void addition() {
+        assertEquals(14, addition.add(8, 8));
+    }
+}
